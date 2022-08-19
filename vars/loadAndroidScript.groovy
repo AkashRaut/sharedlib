23b6c5f976a53f663571eeec39c3af05cf97514b
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2022-08-19 20:09:54 +0530

    Update loadAndroidScript.groovy

--- vars/loadAndroidScript.groovy
+++ vars/loadAndroidScript.groovy
@@ -1,5 +1,3 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "android/${config.name}"    
-  writeFile file: "${config.name}", text: scriptcontents 
-  sh "chmod a+x ./${config.name}"
+  def scriptcontents = libraryResource "android"    
 } 
