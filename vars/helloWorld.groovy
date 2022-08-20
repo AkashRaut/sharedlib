b8483e3ea6f9d3cc03185376682a3c92b3375d20
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2022-08-20 19:32:04 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,4 @@
 def call() {
-		   echo "Cloning the ${ProjectName} project from GitLab"
-	       git url: 'https://github.com/AkashRaut/appcenter-sampleapp-android.git', branch: 'master'
-		   echo "Code has been checked out into ${JENKINS_HOME}/workspace/${JOB_NAME} workspace..!"
+    loadAndroidScript(name: 'hi.sh')
+    sh "./hello-world.sh"
 }
