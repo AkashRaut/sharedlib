b7b5e410debed514927c6b16aa2a9d4e34d75d63
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2022-08-22 18:03:51 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,6 +1,23 @@
 def call() {      
-sh 'npm cache clean --force' 
-		   echo "Cloning the ${ProjectName} project from GitLab"
-	       git url: 'https://github.com/AkashRaut/appcenter-sampleapp-android.git', branch: 'master'
+
+                sh "echo ${env.gitlabSourceBranch}"
+                script {
+                    if ("${env.gitlabSourceBranch}" == 'release') {
+
+           sh 'npm cache clean --force' 
+           echo "release"
+		   echo "Cloning the ${ProjectName} project from Github"
+	       git url: 'https://github.com/AkashRaut/sharedlib.git', branch: 'release', credentialsId: '1'
 		   echo "Code has been checked out into ${JENKINS_HOME}/workspace/${JOB_NAME} workspace..!"
+                    
+                        
+                    }  else {
+ 
+           sh 'npm cache clean --force' 
+           echo "uat"
+		   echo "Cloning the ${ProjectName} project from Github"
+	       git url: 'https://github.com/AkashRaut/sharedlib.git', branch: 'uat', credentialsId: '1'
+		   echo "Code has been checked out into ${JENKINS_HOME}/workspace/${JOB_NAME} workspace..!"
+                    }
+                    }
 }
